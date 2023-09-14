a215798f58137867b4a59a14de164032cf057706
marcocampisi <marco.campisi@outlook.it>
2023-09-14 16:48:21 +0200

    Add CRUD operation

--- app/Http/Controllers/ComicController.php
+++ app/Http/Controllers/ComicController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Comic;
 
 class ComicController extends Controller
 {
@@ -11,7 +12,8 @@ class ComicController extends Controller
      */
     public function index()
     {
-        //
+        $comics = Comic::all();
+        return view('comics.index', compact('comics'));
     }
 
     /**
@@ -19,7 +21,7 @@ class ComicController extends Controller
      */
     public function create()
     {
-        //
+        return view('comics.create');
     }
 
     /**
@@ -27,15 +29,30 @@ class ComicController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $formData = $request->all();
+
+        $comic = new Comic();
+        $comic->title = $formData['title'];
+        $comic->description = $formData['description'];
+        $comic->thumb = $formData['thumb'];
+        $comic->price = $formData['price'];
+        $comic->series = $formData['series'];
+        $comic->sale_date = $formData['sale_date'];
+        $comic->type = $formData['type'];
+        $comic->artists = $formData['artists'];
+        $comic->writers = $formData['writers'];
+
+        $comic->save();
+
+        return redirect()->route('comics.index')->with('success', 'Comic created successfully.');
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(Comic $comic)
     {
-        //
+        return view('comics.show', compact('comic'));
     }
 
     /**
--- database/migrations/2023_09_14_140533_create_comics_table.php
+++ database/migrations/2023_09_14_140533_create_comics_table.php
@@ -14,9 +14,14 @@ return new class extends Migration
         Schema::create('comics', function (Blueprint $table) {
             $table->id();
             $table->string('title');
-            $table->string('author');
             $table->text('description');
+            $table->string('thumb');
             $table->decimal('price', 8, 2);
+            $table->string('series');
+            $table->date('sale_date');
+            $table->string('type');
+            $table->json('artists');
+            $table->json('writers');
             $table->timestamps();
         });
     }
